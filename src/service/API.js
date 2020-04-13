c9cd32901410f4d11be0c0b16e24dd5c5e255c3b
PedroConcejo <pc_89@hotmail.es>
2020-04-13 10:50:26 +0100

    Api

--- src/service/API.js
+++ src/service/API.js
@@ -1,9 +1,23 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: process.env.VUE_APP_API_URL,
-//   headers: {
-//     Accept: "application/json",
-//     "Content-Type": "application/json"
-//   }
-// });
+const API = axios.create({
+    baseURL: "https://opentdb.com/api.php?",
+    headers: {
+     Accept: "application/json",
+     "Content-Type": "application/json"
+   }
+ });
+
+ export default {
+    //Auth
+    async getToken() {
+      const response = await API.get("/command=request");
+      return response.data;
+    },
+    async getQuestion() {
+        const response = await API.get(`amount=10&token=${YOURTOKENHERE}&category=12&difficulty=medium&type=boolean`);
+        return response.data;
+      },
+}
+
+https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=boolean
