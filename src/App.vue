c3b8bc78ccab9981ccd613a8f5c810bcf4019208
calaverita <herrerobego10@gmail.com>
2020-04-13 12:31:57 +0100

    checkbox

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div>
     <Navbar />
     <QuestionsViews />
+    <Filter />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
 import QuestionsViews from "@/views/QuestionsViews.vue";
+import Filter from "@/components/Filter.vue";
 
 export default {
   name: "App",
+
   components: {
     QuestionsViews,
-    Navbar
+    Navbar,
+    Filter
   }
 };
 </script>
---
+++ src/components/Filter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>hola</h1>
+    <v-container fluid>
+      <p>{{ radios || "null" }}</p>
+      <v-radio-group v-model="radios" :mandatory="false">
+        <v-radio
+          :label="category.name"
+          :value="category.id"
+          v-for="category in categories"
+          :key="category.id"
+        ></v-radio>
+      </v-radio-group>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import Api from "@/service/API";
+
+export default {
+  data() {
+    return {
+      category: []
+    };
+  },
+  methods: {
+    async generateCategory() {
+      const categories = await Api.getAllCategory();
+      this.category = categories;
+    }
+  },
+  mounted() {
+    this.generateCategory();
+  }
+};
+</script>
